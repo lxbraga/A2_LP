535476b1f72c6007e222425eb0b78a08f70e2925
Victor <70376108+victorbombarda@users.noreply.github.com>
2020-12-07 19:09:08 -0300

    Primeira analise de visualização - Fifa
    De 3 visualizações da base da Fifa, foi dado um commit para salvar a primera delas.

--- main.py
+++ main.py
@@ -1,4 +1,65 @@
 from Data_Cleaning import CleanFifa as dc
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn import datasets, linear_model, metrics, model_selection
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
-df_fifa = dc().fetcher()
+
+fifa = dc().fetcher()
+
+## Ordenando os valores e agroupando eles
+# Agrupamos por time e ordenamos em ordem decrescente para descobrirmos os times no topo e no final em relação ao aproveitamento de pênaltis.#
+fifa_penalties_club = fifa.groupby(["Club"]).mean()
+fifa_penalties_club = fifa_penalties_club.sort_values(by = "Penalties", ascending = False)
+
+#Agora que sabemos os times no topo e no final da tabela de pênaltis, podemos filtrar cada um; fifa_mp para Melhor Pênalti e fifa_pp para Pior Pênalti.
+fifa_mp = fifa[fifa["Club"].isin(["Grêmio", "Internacional","FC Schalke 04", "Besiktas JK", "Atlético Mineiro"])]
+fifa_pp = fifa[fifa["Club"].isin(["IF Brommapojkarna", "FC Nordsjælland","Trelleborgs FF", "Bray Wanderers", "Derry City"])]
+
+## FINALIZAÇÃO por time - melhor aproveitamento e pior
+fifa_mp= fifa_mp.sort_values(by = "Finishing")
+fifa_pp= fifa_pp.sort_values(by = "Finishing")
+fifa_mp["ID"] = range(len(fifa_mp["Name"]))
+fifa_pp["ID"] = range(len(fifa_pp["Name"]))
+sns.lineplot(x = fifa_mp["ID"], y =fifa_mp["Finishing"], label = "Melhores times - finalizações")
+sns.lineplot(x = fifa_pp["ID"], y =fifa_pp["Finishing"], label = "Piores times - finalizações")
+del fifa_mp["ID"]
+del fifa_pp["ID"]
+
+## CURVA por time - melhor aproveitamento e pior
+fifa_mp= fifa_mp.sort_values(by = "Curve")
+fifa_pp= fifa_pp.sort_values(by = "Curve")
+fifa_mp["ID"] = range(len(fifa_mp["Name"]))
+fifa_pp["ID"] = range(len(fifa_pp["Name"]))
+sns.lineplot(x = fifa_mp["ID"], y =fifa_mp["Curve"],label = "Melhores times - curvatura do chute")
+sns.lineplot(x = fifa_pp["ID"], y =fifa_pp["Curve"],label = "Piores times - curvatura do chute")
+del fifa_mp["ID"]
+del fifa_pp["ID"]
+
+## FORÇA por time - melhor aproveitamento e pior
+fifa_mp= fifa_mp.sort_values(by = "Strength")
+fifa_pp= fifa_pp.sort_values(by = "Strength")
+fifa_mp["ID"] = range(len(fifa_mp["Name"]))
+fifa_pp["ID"] = range(len(fifa_pp["Name"]))
+sns.lineplot(x = fifa_mp["ID"], y =fifa_mp["Strength"],label = "Melhores times - força do chute")
+sns.lineplot(x = fifa_pp["ID"], y =fifa_pp["Strength"],label = "Piores times - força do chute")
+del fifa_mp["ID"]
+del fifa_pp["ID"]
+
+## AGRESSÃO por time - melhor aproveitamento e pior
+fifa_mp= fifa_mp.sort_values(by = "Aggression")
+fifa_pp= fifa_pp.sort_values(by = "Aggression")
+fifa_mp["ID"] = range(len(fifa_mp["Name"]))
+fifa_pp["ID"] = range(len(fifa_pp["Name"]))
+sns.lineplot(x = fifa_mp["ID"], y =fifa_mp["Aggression"],label = "Melhores times - agressão do jogador")
+sns.lineplot(x = fifa_pp["ID"], y =fifa_pp["Aggression"],label = "Piores times - agressão do jogador")
+del fifa_mp["ID"]
+del fifa_pp["ID"]
+
+
+
 

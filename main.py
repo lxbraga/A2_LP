81f6edefe3199aca0b28ae78b5f3a3223164e03e
Victor <70376108+victorbombarda@users.noreply.github.com>
2020-12-08 05:34:57 -0300

    Erros consertados
    Após alguns commits equivocados, aqui está o main consertado

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import warnings
+import geopandas as gpd
+warnings.filterwarnings("ignore")
 
 covid = dfc().fetcher()
 fifa = dff().fetcher()
@@ -58,19 +61,19 @@ fifa.sort_values(by='SprintSpeed',ascending = False )
 fig = plt.figure()
 ax = plt.subplot(111)
 fig11 = sns.lineplot(x="Wage", y="Age",data=fifa,label="Age", ci= None)
-fig.savefig('imagens/FIFA/vis_salarios/fig11.png')
+fig.savefig('imagens/fifa/vis_salarios/fig11.png')
 
     # Visualização salário por overall
 fig = plt.figure()
 ax = plt.subplot(111)
 fig12 = sns.lineplot(x="Wage", y="Overall",data=fifa,label="Overall", ci= None)
-fig.savefig('imagens/FIFA/vis_salarios/fig12.png')
+fig.savefig('imagens/fifa/vis_salarios/fig12.png')
 
     # Visualização salário por potential
 fig = plt.figure()
 ax = plt.subplot(111)
 fig13 = sns.lineplot(x="Wage", y="Potential",data=fifa,label="Potential", ci= None)
-fig.savefig('imagens/FIFA/vis_salarios/fig13.png')
+fig.savefig('imagens/fifa/vis_salarios/fig13.png')
 
 #PRIMEIRA ANALISE DO FIFA
 ## Ordenando os valores e agroupando eles
@@ -85,7 +88,7 @@ fifa_pp = fifa[fifa["Club"].isin(["IF Brommapojkarna", "FC Nordsjælland","Trell
 fig = plt.figure()
 ax = plt.subplot(111)
 fig_errada = sns.lineplot(x = fifa_mp["ID"], y =fifa_mp["Finishing"], label = "Melhores times - finalizações"), sns.lineplot(x = fifa_pp["ID"], y =fifa_pp["Finishing"], label = "Piores times - finalizações")
-fig.savefig('imagens/FIFA/aprov_penaltis/ErradoPenalti-Finalizacao.png')
+fig.savefig('imagens/fifa/aprov_penaltis/ErradoPenalti-Finalizacao.png')
 
 
 
@@ -100,7 +103,7 @@ fifa_pp["ID"] = range(len(fifa_pp["Name"]))
 fig = plt.figure()
 ax = plt.subplot(111)
 fig1 = sns.lineplot(x = fifa_mp["ID"], y =fifa_mp["Finishing"], label = "Melhores times - finalizações"), sns.lineplot(x = fifa_pp["ID"], y =fifa_pp["Finishing"], label = "Piores times - finalizações")
-fig.savefig('imagens/FIFA/aprov_penaltis/Finalizacao.png')
+fig.savefig('imagens/fifa/aprov_penaltis/Finalizacao.png')
 del fifa_mp["ID"]
 del fifa_pp["ID"]
 
@@ -112,7 +115,7 @@ fifa_pp["ID"] = range(len(fifa_pp["Name"]))
 fig = plt.figure()
 ax = plt.subplot(111)
 fig2 = sns.lineplot(x = fifa_mp["ID"], y =fifa_mp["Curve"],label = "Melhores times - curvatura do chute"), sns.lineplot(x = fifa_pp["ID"], y =fifa_pp["Curve"],label = "Piores times - curvatura do chute")
-fig.savefig('imagens/FIFA/aprov_penaltis/Curvatura.png')
+fig.savefig('imagens/fifa/aprov_penaltis/Curvatura.png')
 del fifa_mp["ID"]
 del fifa_pp["ID"]
 
@@ -124,7 +127,7 @@ fifa_pp["ID"] = range(len(fifa_pp["Name"]))
 fig = plt.figure()
 ax = plt.subplot(111)
 fig3 = sns.lineplot(x = fifa_mp["ID"], y =fifa_mp["Strength"],label = "Melhores times - força do chute"), sns.lineplot(x = fifa_pp["ID"], y =fifa_pp["Strength"],label = "Piores times - força do chute")
-fig.savefig('imagens/FIFA/aprov_penaltis/ForcaDoChute.png')
+fig.savefig('imagens/fifa/aprov_penaltis/ForcaDoChute.png')
 del fifa_mp["ID"]
 del fifa_pp["ID"]
 
@@ -136,7 +139,7 @@ fifa_pp["ID"] = range(len(fifa_pp["Name"]))
 fig = plt.figure()
 ax = plt.subplot(111)
 fig4 = sns.lineplot(x = fifa_mp["ID"], y =fifa_mp["Aggression"],label = "Melhores times - agressão do jogador"), sns.lineplot(x = fifa_pp["ID"], y =fifa_pp["Aggression"],label = "Piores times - agressão do jogador")
-fig.savefig('imagens/FIFA/aprov_penaltis/Agressao.png')
+fig.savefig('imagens/fifa/aprov_penaltis/Agressao.png')
 del fifa_mp["ID"]
 del fifa_pp["ID"]
 
@@ -149,7 +152,7 @@ SalarioPorIdade["Age"] = range(16,42)
 fig = plt.figure()
 ax = plt.subplot(111)
 fig5 = sns.lineplot(x = SalarioPorIdade["Age"] , y =SalarioPorIdade["mean"],label = "Média do salário por Idade", color = "red")
-fig.savefig('imagens/FIFA/analise_idade/Salario_idade.png')
+fig.savefig('imagens/fifa/analise_idade/Salario_idade.png')
 
 #NOTA GERAL
 RatingPorIdade = fifa.groupby('Age').Overall.agg(['min','max','mean'])
@@ -157,7 +160,7 @@ RatingPorIdade["Age"] = range(16,42)
 fig = plt.figure()
 ax = plt.subplot(111)
 fig6 = sns.lineplot(x = RatingPorIdade["Age"] , y =RatingPorIdade["mean"],label = "Média da nota geral por Idade", color = "blue" )
-fig.savefig('imagens/FIFA/analise_idade/NotaGeral_idade.png')
+fig.savefig('imagens/fifa/analise_idade/NotaGeral_idade.png')
 
 #STAMINA
 StaminaPorIdade = fifa.groupby('Age').Stamina.agg(['min','max','mean'])
@@ -165,7 +168,7 @@ StaminaPorIdade ["Age"] = range(16,42)
 fig = plt.figure()
 ax = plt.subplot(111)
 fig7 = sns.lineplot(x = StaminaPorIdade ["Age"] , y =StaminaPorIdade ["mean"],label = "Stamina por Idade", color = "green" )
-fig.savefig('imagens/FIFA/analise_idade/Stamina_idade.png')
+fig.savefig('imagens/fifa/analise_idade/Stamina_idade.png')
 
 #JUMPING - PULO
 PuloPorIdade = fifa.groupby('Age').Jumping.agg(['min','max','mean'])
@@ -173,7 +176,7 @@ PuloPorIdade["Age"] = range(16,42)
 fig = plt.figure()
 ax = plt.subplot(111)
 fig8 = sns.lineplot(x = PuloPorIdade["Age"] , y =PuloPorIdade["mean"],label = "Pulo por Idade", color = "black" )
-fig.savefig('imagens/FIFA/analise_idade/Pulo_idade.png')
+fig.savefig('imagens/fifa/analise_idade/Pulo_idade.png')
 
 #AGILITY - AGILIDADE
 AgilidadePorIdade = fifa.groupby('Age').Agility.agg(['min','max','mean'])
@@ -181,7 +184,7 @@ AgilidadePorIdade["Age"] = range(16,42)
 fig = plt.figure()
 ax = plt.subplot(111)
 fig9 = sns.lineplot(x = AgilidadePorIdade["Age"] , y =AgilidadePorIdade["mean"],label = "Agilidade por Idade", color = "purple" )
-fig.savefig('imagens/FIFA/analise_idade/Agilidade_idade.png')
+fig.savefig('imagens/fifa/analise_idade/Agilidade_idade.png')
 
 #WEIGHT - PESO
 PesoPorIdade = fifa.groupby('Age').Weight.agg(['min','max','mean'])
@@ -189,7 +192,7 @@ PesoPorIdade["Age"] = range(16,42)
 fig = plt.figure()
 ax = plt.subplot(111)
 fig10 =sns.lineplot(x = PesoPorIdade["Age"] , y =PesoPorIdade["mean"],label = "Peso por Idade", color = "cyan" )
-fig.savefig('imagens/FIFA/analise_idade/Peso_idade.png')
+fig.savefig('imagens/fifa/analise_idade/Peso_idade.png')
 
 ## CRIANDO matriz de correlação e gráfico heatmap
 # Avaliando correlação entre atributos
@@ -225,7 +228,7 @@ fig = plt.Figure()
 fig.set_canvas(plt.gcf().canvas)
 ax = plt.subplot(111)
 fig14 = plt.plot(x_train, model.predict(x_train),color="blue")
-fig.savefig('imagens/FIFA/regressoes/regressao_linear1_treino.png')
+fig.savefig('imagens/fifa/regressoes/regressao_linear1_treino.png')
 
     # Visualizando dataset de teste
     # Onde os dados serão previstos com base no conjunto de treino, medindo a eficiência de previsão
@@ -236,7 +239,7 @@ fig = plt.Figure()
 fig.set_canvas(plt.gcf().canvas)
 ax = plt.subplot(111)
 fig15 = plt.plot(x_train, model.predict(x_train),color="blue")
-fig.savefig('imagens/FIFA/regressoes/regressao_linear1_teste.png')
+fig.savefig('imagens/fifa/regressoes/regressao_linear1_teste.png')
 
     # Encontrando interseção da linha de regressão
 model.intercept_
@@ -275,7 +278,7 @@ fig = plt.Figure()
 fig.set_canvas(plt.gcf().canvas)
 ax = plt.subplot(111)
 fig16 = plt.plot(x_train, model.predict(x_train),color="blue")
-fig.savefig('imagens/FIFA/regressoes/regressao_linear2_treino.png')
+fig.savefig('imagens/fifa/regressoes/regressao_linear2_treino.png')
 
     # Visualizando dataset de teste
     # Onde os dados serão previstos com base no conjunto de treino, medindo a eficiência de previsão
@@ -286,7 +289,7 @@ fig = plt.Figure()
 fig.set_canvas(plt.gcf().canvas)
 ax = plt.subplot(111)
 fig17 = plt.plot(x_train, model.predict(x_train),color="blue")
-fig.savefig('imagens/FIFA/regressoes/regressao_linear2_teste.png')
+fig.savefig('imagens/fifa/regressoes/regressao_linear2_teste.png')
 
     # Encontrando interseção da linha de regressão
 model.intercept_
@@ -315,11 +318,30 @@ fig = plt.Figure()
 fig.set_canvas(plt.gcf().canvas)
 ax = plt.subplot(111)
 fig18 = plt.scatter(y_test,y_pred), plt.xlabel("Actual Wage"), plt.ylabel("Predicted Wage")
-fig.savefig('imagens/FIFA/regressoes/regressao_multipla_teste.png')
+fig.savefig('imagens/fifa/regressoes/regressao_multipla_teste.png')
 
     # Afinidade entre os dados avaliados
     # Há uma afinidade de 0.35 entre o salário dos jogadores e os demais dados avaliados
 model.score(x_train,y_train)
+
+## COVID ------------------------------------------
+covid["Lat"] = ""
+covid["Long"] = "" #Separa a Latitude da Longitude do arquivo original fornecido
+for i in range(len(covid["Centroid"])):
+    covid["Centroid"][i] = covid["Centroid"][i][6:-1]
+    covid["Lat"][i] = covid["Centroid"][i].split(" ")[0] 
+    covid["Long"][i] = covid["Centroid"][i].split(" ")[1] 
+
+#Achamos no mapa o lugar dos aeroportos da basex
+gdf = gpd.GeoDataFrame(covid, geometry=gpd.points_from_xy(covid["Lat"],covid["Long"]))
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+ax = world.plot(column='PercentOfBaseline',color='white', edgecolor='black',figsize=(15,9))
+ax.set_title('Aeroportos analisados na tabela - Covid', fontdict= {'fontsize':25})
+ax.set_axis_off()   
+ax.get_figure()
+gdf.plot(ax=ax, color='red')
+plt.savefig("imagens/Covid/mapa .png")
+plt.show() #Printa cada aeroporto como uma bolinha vermelha no mapa
 
 ## IMPACTO DO COVID 19 NOS AEROPORTOS:
     
@@ -366,24 +388,6 @@ fig.savefig('imagens/vis_covid/trafego_por_pais.png')
 pais = {'United States of America (the)': 0, 'Canada': 1, 'Australia':2, 'Chile':3}
 covid["Country"] = [pais[linha] for linha in covid["Country"]]
 
-## COVID ------------------------------------------
-covid["Lat"] = ""
-covid["Long"] = "" #Separa a Latitude da Longitude do arquivo original fornecido
-for i in range(len(covid["Centroid"])):
-    covid["Centroid"][i] = covid["Centroid"][i][6:-1]
-    covid["Lat"][i] = covid["Centroid"][i].split(" ")[0] 
-    covid["Long"][i] = covid["Centroid"][i].split(" ")[1] 
-
-#Achamos no mapa o lugar dos aeroportos da basex
-gdf = gpd.GeoDataFrame(covid, geometry=gpd.points_from_xy(covid["Lat"],covid["Long"]))
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-ax = world.plot(column='PercentOfBaseline',color='white', edgecolor='black',figsize=(15,9))
-ax.set_title('Aeroportos analisados na tabela - Covid', fontdict= {'fontsize':25})
-ax.set_axis_off()   
-ax.get_figure()
-gdf.plot(ax=ax, color='red')
-plt.savefig("imagens/Covid/mapa .png")
-plt.show() #Printa cada aeroporto como uma bolinha vermelha no mapa
 
 covid = covid.drop(["Date"], axis=1)
 X = covid["Country"].values.reshape(-1,1)

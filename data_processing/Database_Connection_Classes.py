34d8463da9f061aba314ef8cdba337b969811521
lxbraga <55038239+lxbraga@users.noreply.github.com>
2020-12-08 03:49:51 -0300

    Escondendo o password

--- data_processing/Database_Connection_Classes.py
+++ data_processing/Database_Connection_Classes.py
@@ -21,7 +21,8 @@ class Connection:
         self.server = "fgv-db-server.database.windows.net" 
         self.db = "fgv-db"
         self.user = "student" 
-        self.pwd = "@dsInf123"
+        with open("PWD") as f: #Arquivo com a senha para passar implicitamente
+            self.pwd = f.read()
         self.driver = "{ODBC Driver 17 for SQL Server}"
         
     def connection(self):
